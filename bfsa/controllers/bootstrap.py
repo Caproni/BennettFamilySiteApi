f43d6bc9e16c56c1870ad306ab54af37ed6df2a7
Edmund Bennett <Edmund.Bennett@ghd.com>
2022-09-27 11:04:30 +0100

    Tags for FastAPI endpoints

--- bfsa/controllers/bootstrap.py
+++ bfsa/controllers/bootstrap.py
@@ -34,24 +34,24 @@ host = "localhost"
 
 server = FastAPI()
 
-server.include_router(admin_controller.router)
-server.include_router(authentication_controller.router)
+server.include_router(admin_controller.router, tags=["Administration"])
+server.include_router(authentication_controller.router, tags=["Authentication"])
 
-server.include_router(family_tree_person_controller.router)
-server.include_router(family_tree_relationship_controller.router)
-server.include_router(family_tree_data_source_controller.router)
+server.include_router(family_tree_person_controller.router, tags=["Family Tree People"])
+server.include_router(family_tree_relationship_controller.router, tags=["Family Tree Relationships"])
+server.include_router(family_tree_data_source_controller.router, tags=["Family Tree Data Sources"])
 
-server.include_router(media_controller.router)
+server.include_router(media_controller.router, tags=["Media"])
 
-server.include_router(photos_controller.router)
+server.include_router(photos_controller.router, tags=["Photos"])
 
-server.include_router(recipe_controller.router)
-server.include_router(recipe_step_controller.router)
-server.include_router(ingredient_controller.router)
-server.include_router(ingredient_usage_controller.router)
-server.include_router(equipment_controller.router)
-server.include_router(equipment_usage_controller.router)
-server.include_router(recipe_detail_controller.router)
+server.include_router(recipe_controller.router, tags=["Recipes"])
+server.include_router(recipe_step_controller.router, tags=["Recipe Steps"])
+server.include_router(ingredient_controller.router, tags=["Ingredients"])
+server.include_router(ingredient_usage_controller.router, tags=["Ingredient Usage"])
+server.include_router(equipment_controller.router, tags=["Equipment"])
+server.include_router(equipment_usage_controller.router, tags=["Equipment Usage"])
+server.include_router(recipe_detail_controller.router, tags=["Recipe Details"])
 
 origins = [
     "http://localhost",
